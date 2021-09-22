daeea2a82005d3aab59723124a091e2eefdf4bc6
wachirawit.c <wachirawit.chankaew@gmail.com>
2021-09-22 20:52:02 +0700

    Add data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,45 @@
 <template>
-  Home
+  <div class="row">
+    <div class="col-md-12" v-for="item in sources.Brewing" :key="item.Name">
+      <div class="row mb-2">
+        <div class="col-12">
+          <span>Name</span>
+          <div>{{ item.Name }}</div>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div class="col-2">
+          <span class="title">ปริมาณกาแฟ (g)</span>
+          <div>{{ item.CoffeeWeightInGram }}</div>
+        </div>
+        <div class="col-2">
+          <span class="title">Ratio</span>
+          <div>{{ item.Ratio }}</div>
+        </div>
+        <div class="col-2">
+          <span class="title">Total Water (ml)</span>
+          <div>{{ item.CoffeeWeightInGram * item.Ratio }}</div>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div class="col-2 text-center" v-for="pour in item.Pours" :key="pour.TimeInSecond">
+          <div>Time {{pour.TimeInSecond}} seconds</div>
+          <div>Water {{pour.WaterInMilliliter}} ml</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import drip from './drip.json'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  data: () => ({
+    sources: drip,
+    summaries: []
+  })
 }
 </script>
 
