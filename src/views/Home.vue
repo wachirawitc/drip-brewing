8cd85318e6cf5f8abc0aefadf50fac4e39c4300a
wachirawit.c <wachirawit.chankaew@gmail.com>
2021-09-22 21:20:25 +0700

    Add data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,31 +4,30 @@
       <div class="row mt-2 mb-2">
         <div class="col-12">
           <span class="title">Name</span>
-          <div>{{ item.Name }}</div>
+          <div class="detail">{{ item.Name }}</div>
         </div>
       </div>
       <div class="row mb-4">
         <div class="col-3 text-center">
           <span class="title">ปริมาณกาแฟ (g)</span>
-          <div>{{ item.CoffeeWeightInGram }}</div>
+          <div class="detail">{{ item.CoffeeWeightInGram }}</div>
         </div>
         <div class="col-2 text-center">
           <span class="title">Ratio</span>
-          <div>{{ item.Ratio }}</div>
+          <div class="detail">{{ item.Ratio }}</div>
         </div>
         <div class="col-3 text-center">
           <span class="title">Total Water (ml)</span>
-          <div>{{ item.CoffeeWeightInGram * item.Ratio }}</div>
+          <div class="detail">{{ item.CoffeeWeightInGram * item.Ratio }}</div>
         </div>
         <div class="col-3 text-center">
           <span class="title">Total Times (minutes)</span>
-          <div>{{ getTotalTimes(item.Pours) }}</div>
+          <div class="detail">{{ getTotalTimes(item.Pours) }}</div>
         </div>
       </div>
       <div class="row mb-4">
         <div class="col-2 text-center" v-for="pour in item.Pours" :key="pour.TimeInSecond">
-          <div>Time {{ pour.TimeInSecond }} seconds</div>
-          <div>Water {{ pour.WaterInMilliliter }} ml</div>
+          <div class="pour-detail">{{ pour.TimeInSecond }}s/{{ pour.WaterInMilliliter }}ml</div>
         </div>
       </div>
     </div>
