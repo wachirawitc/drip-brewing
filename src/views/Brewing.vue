e9c543624a7f462068c0064401e80fd5918d4492
wachirawit.c <wachirawit.chankaew@gmail.com>
2021-09-23 09:22:51 +0700

    Update view

--- src/views/Brewing.vue
+++ src/views/Brewing.vue
@@ -1,38 +1,37 @@
 <template>
   <div class="row">
-    <div class="col-md-12" v-for="item in sources.Brewing" :key="item.Name">
+    <div class="col-md-12">
       <div class="row mt-2 mb-2">
         <div class="col-12">
           <span class="title">Name</span>
-          <div class="detail">{{ item.Name }}</div>
+          <div class="detail">{{ brewing.Name }}</div>
         </div>
       </div>
       <div class="row mb-4">
         <div class="col-3 text-center">
-          <span class="title">ปริมาณกาแฟ (g)</span>
-          <div class="detail">{{ item.CoffeeWeightInGram }}</div>
+          <span class="title">Coffee weight (g)</span>
+          <div class="detail">{{ brewing.CoffeeWeightInGram }}</div>
         </div>
         <div class="col-2 text-center">
           <span class="title">Ratio</span>
-          <div class="detail">{{ item.Ratio }}</div>
+          <div class="detail">{{ brewing.Ratio }}</div>
         </div>
         <div class="col-3 text-center">
           <span class="title">Total Water (ml)</span>
-          <div class="detail">{{ item.CoffeeWeightInGram * item.Ratio }}</div>
+          <div class="detail">{{ brewing.CoffeeWeightInGram * brewing.Ratio }}</div>
         </div>
         <div class="col-3 text-center">
           <span class="title">Total Times (minutes)</span>
-          <div class="detail">{{ getTotalTimes(item.Pours) }}</div>
+          <div class="detail">{{ getTotalTimes(brewing.Pours) }}</div>
         </div>
       </div>
       <div class="row mb-4">
-        <div class="col-md-12">
-          aa
-        </div>
-      </div>
-      <div class="row mb-4">
-        <div class="col-2 text-center" v-for="pour in item.Pours" :key="pour.TimeInSecond">
-          <div class="pour-detail">{{ pour.TimeInSecond }}s/{{ pour.WaterInMilliliter }}ml</div>
+        <div class="col-2 text-center" v-for="pour in information" :key="pour.WaterInMilliliter">
+          <div>
+            <img src="images/img_glass.png">
+          </div>
+          <div class="pour-water">{{ pour.WaterInMilliliter }}ml</div>
+          <div class="pour-time">{{ pour.start }} - {{ pour.end }}</div>
         </div>
       </div>
     </div>
@@ -40,15 +39,35 @@
 </template>
 
 <script>
-
+import moment from 'moment'
 import drip from './drip.json'
 
 export default {
   name: 'Brewing',
   data: () => ({
     sources: drip,
-    summaries: []
+    information: [],
+    brewing: {}
   }),
+  created () {
+    const self = this
+    const brewing = self.sources.Brewing.find(x => x.Id === 1)
+    self.brewing = brewing
+    let lastedWater = 0
+    let fromTime = new Date(0, 0, 0, 0, 0, 0, 0)
+    brewing.Pours.forEach((pour) => {
+      lastedWater = lastedWater + pour.WaterInMilliliter
+      const start = fromTime
+      const end = new Date(0, 0, 0, 0, 0, 0, 0)
+      end.setSeconds(start.getSeconds() + pour.TimeInSecond)
+      self.information.push({
+        start: moment(start).format('mm:ss'),
+        end: moment(end).format('mm:ss'),
+        WaterInMilliliter: lastedWater
+      })
+      fromTime = end
+    })
+  },
   methods: {
     getTotalTimes: function (pours) {
       const data = new Date(0, 0, 0, 0, 0, 0, 0)
@@ -56,7 +75,7 @@ export default {
         const pour = pours[index]
         data.setSeconds(data.getSeconds() + pour.TimeInSecond)
       }
-      return data.getMinutes() + ':' + data.getSeconds()
+      return moment(data).format('mm:ss')
     }
   }
 }
